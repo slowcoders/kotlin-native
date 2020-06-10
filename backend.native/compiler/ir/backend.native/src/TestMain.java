3241b2cf69328eb4bead9c9ea822095e7513ea44
zeedhoon <zeedhoon@gmail.com>
2020-06-10 22:08:42 +0900

    fix args error

--- backend.native/compiler/ir/backend.native/src/TestMain.java
+++ backend.native/compiler/ir/backend.native/src/TestMain.java
@@ -14,7 +14,7 @@ public class TestMain {
      */
     public static void main(String args0[]) {
         String command = "konanc /Users/zeedh/Desktop/ktest/kobjc/libs/zee/src/nativeMain/kotlin/hello.kt" +
-                " -memory-model relaxed -verbose -nomain -p framework -g " +
+                " -memory-model relaxed -verbose -nomain -p framework -g" +
                 " -o /Users/zeedh/Desktop/ktest/kobjc/libs/zee/build/objs/Test2";
         String[] args2 = command.split(" ");
         org.jetbrains.kotlin.cli.utilities.MainKt.main(args2);
