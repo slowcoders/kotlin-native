ec441b826318a376b975ae9b194cc77cf8730989
zeedhoon <zeedhoon@gmail.com>
2020-09-15 18:04:12 +0900

    init v1.4.20-rtgc branch

--- _local/kobjc/libs/zee/make.sh
+++ _local/kobjc/libs/zee/make.sh
@@ -1,3 +1,4 @@
+# kotlin v1.4.20
 ../../../../dist/bin/kotlinc src/nativeMain/kotlin/ -memory-model relaxed \
   -library objcSmoke -library objc_illegal_sharing_with_weak \
   -verbose -nomain -p framework -Xembed-bitcode \
