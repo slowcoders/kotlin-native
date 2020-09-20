8bad24bd6f6be359d531ef45eeb5fafb9acc9ef6
zeedhoon <zeedhoon@gmail.com>
2020-09-20 19:33:54 +0900

    changed compile mode to debug

--- _local/kobjc/libs/zee/make.sh
+++ _local/kobjc/libs/zee/make.sh
@@ -1,4 +1,7 @@
 ../../../../dist/bin/kotlinc src/nativeMain/kotlin/ -memory-model relaxed \
   -library objcSmoke -library objc_illegal_sharing_with_weak \
   -verbose -nomain -p framework -Xembed-bitcode \
-  -opt -linker-option -L. -linker-option -lobjcsmoke -o build/objs/Test2
+  -g -linker-option -L. -linker-option -lobjcsmoke -o build/objs/Test2
+
+# -opt : release
+# -g : debug
